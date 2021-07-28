06859f9fffb20294de72f087b92be65b93cb06f2
Crosimo <jean.deborsu@hotmail.com>
2021-07-28 11:06:30 +0200

    problème

--- resources/views/pages/create_blogs.blade.php
+++ resources/views/pages/create_blogs.blade.php
@@ -21,6 +21,23 @@
     <button type="submit" class="btn btn-primary">Submit</button>
   </form>
 
+  <section class="container">
+      <div class="row">
+          @foreach ($collection as $item)
+          <div class="col-4">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+          </div>
+          @endforeach
+
+      </div>
+  </section>
+
 
 @endsection
 
--- resources/views/template/backoffice.blade.php
+++ resources/views/template/backoffice.blade.php
@@ -54,19 +54,7 @@
 
 
 
-            <!-- Bootstrap core JavaScript
-          ================================================== -->
-          <!-- Placed at the end of the document so the pages load faster -->
-          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-          <script src="{{asset('js/bootstrap.min.js')}}"></script>
-          <script src="{{asset('js/jquery.slicknav.min.js')}}"></script>
-          <script src="{{asset('js/slick.min.js')}}"></script>
-          <script src="{{asset('js/touchswipe.min.js')}}"></script>
-          <script src="{{asset('js/skrollr.min.js')}}"></script>
-          <script src="{{asset('js/jquery.countTo.min.js')}}"></script>
-          <script src="{{asset('js/script.js')}}"></script>
-      </body>
-      </html>
+
 
 
 
